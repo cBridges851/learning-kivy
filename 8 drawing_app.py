ad1bbadaf5d32a32b1c98cf6df93536a184e5c53
Christa Bridges <christa@pebblepad.co.uk>
2021-02-06 16:04:19 +0000

    master Removed commented out code and prints.

--- 8 drawing_app.py
+++ 8 drawing_app.py
@@ -11,11 +11,9 @@ class Touch(Widget):
     def __init__(self, **kwargs):
         super(Touch, self).__init__(**kwargs)
 
-        is_pen_down = False
         with self.canvas:
             Color(0, 0, 0, 1, mode="rgba")
             self.line = Line(points=())
-
 
     def on_touch_down(self, touch):
         with self.canvas:
@@ -23,17 +21,11 @@ class Touch(Widget):
             self.line = Line(points=())
 
     def on_touch_move(self, touch):
-        print(f"Mouse moved {touch}")
         self.line.points.append(touch.pos[0])
         self.line.points.append(touch.pos[1])
-        print(self.line.points)
         with self.canvas:
             Color(0, 0, 0, 1, mode="rgba")
             self.line = Line(points=self.line.points)
-        # self.rect.pos = touch.pos
-
-    # def on_touch_up(self, touch):
-    #     print(f"Mouse up {touch}")
 
 class DrawingApp(App):
     def build(self):
