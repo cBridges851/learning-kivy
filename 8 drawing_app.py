e14886af1ba58f09721dea4894e330bf340d7d38
Christa Bridges <christa@pebblepad.co.uk>
2021-02-06 15:33:11 +0000

    master The user can now draw lines freehand on the canvas.

--- 8 drawing_app.py
+++ 8 drawing_app.py
@@ -3,6 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
 from kivy.core.window import Window
+from kivy.graphics import Line
 
 Window.clearcolor = (1, 1, 1, 1)
 
@@ -10,19 +11,26 @@ class Touch(Widget):
     def __init__(self, **kwargs):
         super(Touch, self).__init__(**kwargs)
 
-        # with self.canvas:
-        #     Color(1, 0.4, 1, 1, mode="rgba")
-        #     self.rect = Rectangle(pos=(0, 0), size=(100, 100))
-        #     Color(0.5, 1, 1, 1, mode="rgba")
+        is_pen_down = False
+        with self.canvas:
+            Color(0, 0, 0, 1, mode="rgba")
+            self.line = Line(points=())
 
 
-    # def on_touch_down(self, touch):
-    #     print(f"Mouse down {touch}")
-    #     # self.rect.pos = touch.pos
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(0, 0, 0, 1, mode="rgba")
+            self.line = Line(points=())
 
-    # def on_touch_move(self, touch):
-    #     print(f"Mouse moved {touch}")
-    #     # self.rect.pos = touch.pos
+    def on_touch_move(self, touch):
+        print(f"Mouse moved {touch}")
+        self.line.points.append(touch.pos[0])
+        self.line.points.append(touch.pos[1])
+        print(self.line.points)
+        with self.canvas:
+            Color(0, 0, 0, 1, mode="rgba")
+            self.line = Line(points=self.line.points)
+        # self.rect.pos = touch.pos
 
     # def on_touch_up(self, touch):
     #     print(f"Mouse up {touch}")
